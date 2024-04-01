432630ffb12b8d17f4e4604387d3a8eee41f75df
Nikita Sorokin <maxsuper2281337@gmail.com>
2024-04-01 10:22:29 +0300

    Проект создан

---
+++ quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quiz
+//
+//  Created by Никита Сорокин on 01.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
